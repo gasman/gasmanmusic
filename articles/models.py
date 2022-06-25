4998fd7677d29af285cd66e42df8231dbc28097d
Matt Westcott <matt@west.co.tt>
2022-06-26 00:53:09 +0100

    Add article pages

---
+++ articles/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from wagtail import blocks
+from wagtail.admin import panels
+from wagtail.models import Page
+from wagtail.fields import StreamField
+
+class ArticlePage(Page):
+    body = StreamField([
+        ("paragraph", blocks.RichTextBlock()),
+        ("embed", blocks.RawHTMLBlock()),
+    ])
+
+    content_panels = Page.content_panels + [
+        panels.FieldPanel("body"),
+    ]

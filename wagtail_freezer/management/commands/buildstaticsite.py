8324fba516339bcd8620618acdbc13ececd3048d
Matt Westcott <matt@west.co.tt>
2022-06-26 23:13:01 +0100

    Handle static files in buildstaticsite

--- wagtail_freezer/management/commands/buildstaticsite.py
+++ wagtail_freezer/management/commands/buildstaticsite.py
@@ -1,11 +1,13 @@
 from pathlib import Path
-from shutil import rmtree
+from shutil import copyfile, rmtree
 
+from bs4 import BeautifulSoup
 from django.core.exceptions import ImproperlyConfigured
 from django.core.handlers.base import BaseHandler
 from django.core.handlers.wsgi import WSGIRequest
 from django.core.management.base import BaseCommand
 from django.conf import settings
+from django.contrib.staticfiles import finders
 from wagtail.models import Site
 
 class Command(BaseCommand):
@@ -17,9 +19,13 @@ class Command(BaseCommand):
         except AttributeError:
             raise ImproperlyConfigured("FREEZER_BUILD_DIR must be defined in settings")
 
+        static_assets_url = getattr(settings, "STATIC_URL", "")
+        copy_static_assets = static_assets_url.startswith("/")
+
         sites = Site.objects.all()
 
         for site in sites:
+            static_assets = set()
             site_static_root = static_root / site.hostname
             rmtree(site_static_root, ignore_errors=True)
 
@@ -52,4 +58,20 @@ class Command(BaseCommand):
                 page_path.mkdir(parents=True)
                 with (page_path / "index.html").open(mode='wb') as f:
                     f.write(response.content)
- 
+
+                if copy_static_assets:
+                    soup = BeautifulSoup(response.content, "html.parser")
+                    for elem in soup.find_all(lambda tag:('href' in tag.attrs or 'src' in tag.attrs)):
+                        for attr in ('href', 'src'):
+                            url = elem.get(attr, "")
+                            if url.startswith(static_assets_url):
+                                static_assets.add(url[len(static_assets_url):])
+
+            if static_assets:
+                destination_base_path = site_static_root / static_assets_url[1:]
+                for asset_path in static_assets:
+                    source_file = finders.find(asset_path)
+                    if source_file:
+                        destination_path = destination_base_path / asset_path
+                        destination_path.parent.mkdir(parents=True, exist_ok=True)
+                        copyfile(source_file, destination_path)
